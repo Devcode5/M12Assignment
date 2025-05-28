55f1d943fd60db6578f48ec836785e1edfc1a52e
Aqila N <aqila.nasiry@gmail.com>
2025-05-28 10:27:52 -0700

    Design an Inventory class that can hold the information and calculate data for items in a retail store's inventory. class specification must be saved in Inventory.h.

--- M12Assignment/Inventory.h
+++ M12Assignment/Inventory.h
@@ -2,6 +2,9 @@
 //  M12Assignment
 //  Created by Aqila Nasiry on 5/8/25.
 
+/*
+ Design an Inventory class that can hold the information and calculate data for items in a retail store's inventory.
+ */
 
 // Inventory.h
 #ifndef INVENTORY_H
